d1455680db3737b1ff8bd208bb1105cb68893946
Jonathan Fidelis Paul <jfdpaul@gmail.com>
2021-06-21 02:24:59 +0530

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      process.env.DB_URL || 'mongodb://localhost:27017/alphaa-todos',
+      process.env.DB_URL ||
+        'mongodb://root:MyPrCIp3odNHEWGV@localhost:27017/alphaa-todos',
     ),
     MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
   ],
