cf136df114e8bc35b1cce3acdf3e8841a8a194dc
Jonathan Fidelis Paul <jfdpaul@gmail.com>
2021-06-21 02:11:23 +0530

    add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Todo, TodoSchema } from './todo.schema';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/alphaa-todos'),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      process.env.DB_URL || 'mongodb://localhost:27017/alphaa-todos',
+    ),
     MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }]),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3001);
+  console.log(process.env);
+  await app.listen(parseInt(process.env.PORT, 10) || 4001);
 }
 bootstrap();
